223672ea329224eaebf556e02660294bcdb292f6
Sam Osman <Sam Osman>
2016-04-10 21:19:31 -0600

    names

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
  * Main Description: This instantiates the Window class.
  * 
  * @author (Josh Blair | MRU ID: 201402698 | E: jblai698@mtroyal.ca) 
- * @author (Sam Osman | MRU ID: ________ | E: sosma464@mtroyal.ca) 
+ * @author (Sam Osman | MRU ID: 201612464 | E: sosma464@mtroyal.ca) 
  * @author (Jorge Abarca | MRU ID: ________ | E: ______________)
  * @author (Brian ____ | MRU ID: ________ | E: ______________)
  */
